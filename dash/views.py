2cd71b0eb85ad89419bbf56fc71f2629bd48e130
Nithish Kumar <learn.nithishkumar@gmail.com>
2023-07-07 14:12:45 +0530

    final register

--- dash/views.py
+++ dash/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from .forms import SignupForm
 
 # Create your views here.
 def home(request):
@@ -13,7 +14,7 @@ def home(request):
             login(request, user)
             messages.success(request, "You have logged in successfully!")
         else:
-            messages.warning(request, "You are not logged in, please try again.")
+            messages.success(request, "You are not logged in, please try again.")
         return redirect('home')
     else:
         return render(request, 'home.html', {})
@@ -22,3 +23,19 @@ def logout_user(request):
     logout(request)
     messages.success(request, "You have been logged out!")
     return redirect('home')
+
+def register_user(request):
+    if request.method == 'POST':
+        form = SignupForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            user = authenticate(request, username = username, password=password)
+            login(request, user)
+            messages.success(request, "Your account has been created successfully!")
+            return redirect('home')
+    else:
+        form = SignupForm()
+        return render(request, 'register.html', {'form':form})
+    return render(request, 'register.html', {'form':form})
