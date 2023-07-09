948e38739611965155ea48547a3f7d68cb288202
Nithish Kumar <learn.nithishkumar@gmail.com>
2023-07-09 13:28:05 +0530

    delete records

--- dash/views.py
+++ dash/views.py
@@ -51,3 +51,13 @@ def customer_record(request, pk):
     else:
         messages.success(request, "You must be logged in to view the page!")
         return redirect('home')
+    
+def delete_record(request, pk):
+    if request.user.is_authenticated:
+        delete_it = Customer_db.objects.get(id=pk)
+        delete_it.delete()
+        messages.success(request, "Record deleted successfully!")
+        return redirect('home')
+    else:
+        messages.success(request, "You must be logged in to delete the record!")
+        return redirect('home')
